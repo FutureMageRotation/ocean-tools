da909b974258798b7db067997a2fee3e86527278
FutureMageRotation <collins7293236@gmail.com>
2026-05-26 18:36:58 +0300

    Create ExplorationTools.cs

---
+++ src/Features/ExplorationTools.cs
@@ -0,0 +1,12 @@
+using OceanTools.Core;
+
+namespace OceanTools.Features
+{
+    public static class ExplorationTools
+    {
+        public static void Initialize()
+        {
+            Logger.Info("Exploration Tools initialized.");
+        }
+    }
+}
