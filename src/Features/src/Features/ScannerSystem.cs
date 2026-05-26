addeff690b6d51a151ef9e75bcd223cc8307854d
FutureMageRotation <collins7293236@gmail.com>
2026-05-26 18:36:01 +0300

    Create ScannerSystem.cs

---
+++ src/Features/src/Features/ScannerSystem.cs
@@ -0,0 +1,12 @@
+using OceanTools.Core;
+
+namespace OceanTools.Features
+{
+    public static class ScannerSystem
+    {
+        public static void Initialize()
+        {
+            Logger.Info("Scanner System initialized.");
+        }
+    }
+}
