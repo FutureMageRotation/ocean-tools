d12fceeaceb2d7cf5422586d22371405cb934ea5
FutureMageRotation <collins7293236@gmail.com>
2026-05-26 18:36:30 +0300

    Create CompassUI.cs

---
+++ src/Features/CompassUI.cs
@@ -0,0 +1,12 @@
+using OceanTools.Core;
+
+namespace OceanTools.Features
+{
+    public static class CompassUI
+    {
+        public static void Initialize()
+        {
+            Logger.Info("Compass UI initialized.");
+        }
+    }
+}
