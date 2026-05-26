48d93ffff27c227abe2ef1beec8cb6cac068bd4b
FutureMageRotation <collins7293236@gmail.com>
2026-05-26 18:35:47 +0300

    Create NavigationSystem.cs

---
+++ src/Features/NavigationSystem.cs
@@ -0,0 +1,12 @@
+using OceanTools.Core;
+
+namespace OceanTools.Features
+{
+    public static class NavigationSystem
+    {
+        public static void Initialize()
+        {
+            Logger.Info("Navigation System initialized.");
+        }
+    }
+}
