204b83815b9f18c0b557fe12f0892b0fa5adc500
FutureMageRotation <collins7293236@gmail.com>
2026-05-26 18:34:27 +0300

    Create Loader.cs

---
+++ src/Loader.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OceanTools.Core
+{
+    public class Loader
+    {
+        public static void Initialize()
+        {
+            Console.WriteLine("[OceanTools] Loaded successfully.");
+        }
+    }
+}
